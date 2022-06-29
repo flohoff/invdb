556e571407d82ff2b396fa3bfbfd100facc8b0b2
Florian Lohoff <f@zz.de>
2022-06-29 12:23:50 +0200

    backend: Test/ Rewrite 00-test to perl - use Helper

---
+++ backend/test/lib/InvDBTest.pm
@@ -0,0 +1,53 @@
+
+package InvDBTest;
+	use strict;
+	use File::Slurp qw/read_file/;
+	use DBI;
+
+	sub new {
+		my ($class, $args) = @_;
+		my $self={
+			args => $args
+		};
+		bless $self, $class;
+
+		$self->{dbh}=$self->db_open();
+
+		return $self;
+	}
+
+	sub db_open {
+		my ($self) = @_;
+
+		my $dbname=$ENV{POSTGRES_DB};
+		my $dbuser=$ENV{POSTGRES_USER};
+		my $dbpass=$ENV{POSTGRES_PASSWORD};
+		my $dbhost='postgres';
+
+		# "dbi:Pg:dbname=addresses;host=m2.zz.de;port=5433
+		my $dbcontact=sprintf("dbi:Pg:dbname=%s", $dbname);
+		if (defined($dbhost)) {
+			$dbcontact.=sprintf(";host=%s", $dbhost);
+		}
+
+		my $dbh=DBI->connect_cached($dbcontact,
+			$dbuser,
+			$dbpass) || die $DBI::errstr;
+
+		return $dbh;
+	}
+
+	sub db_clean {
+		my ($self) = @_;
+
+		$self->{dbh}->do("drop schema public cascade") || die $DBI::errstr;
+		$self->{dbh}->do("create schema public") || die $DBI::errstr;
+	}
+
+	sub db_schema_add {
+		my ($self, $schemafile) = @_;
+
+		my $sql=read_file($schemafile);
+		$self->{dbh}->do("BEGIN $sql COMMIT;") || die $DBI::errstr;
+	}
+1;
