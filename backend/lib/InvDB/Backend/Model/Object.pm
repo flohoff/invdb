a737d9f3eef9b0cdb75e878d573ebb0b2d412c25
Florian Lohoff <f@zz.de>
2022-07-08 17:47:30 +0200

    Add POST /object - Create new objects

--- backend/lib/InvDB/Backend/Model/Object.pm
+++ backend/lib/InvDB/Backend/Model/Object.pm
@@ -1,7 +1,8 @@
 package InvDB::Backend::Model::Object;
 use strict;
 use Mojo::Base -base, -signatures;
-use Mojo::JSON qw/decode_json/;
+use Mojo::JSON qw/decode_json encode_json/;
+use OSSP::uuid;
 
 has 'pg';
 
@@ -40,12 +41,44 @@ sub get_links_to($self, $id) {
 
 	return \@links;
 }
-sub get ($self, $uuid) {
+
+sub get_with_neighbours($self, $uuid) {
 	my $row=$self->pg->db->select('objects', '*', { uuid => $uuid })->hash;
 	my $object=decode_json($row->{object});
 	$object->{links}{from}=$self->get_links_from($row->{id});
 	$object->{links}{to}=$self->get_links_to($row->{id});
 	return $object;
+}
+
+sub get($self, $uuid) {
+	my $row=$self->pg->db->select('objects', '*', { uuid => $uuid })->hash;
+	my $object=decode_json($row->{object});
+	return $object;
+}
+
+sub uuid_v4($self) {
+	my $uuid=new OSSP::uuid;
+	$uuid->make("v4");
+	return $uuid->export("str");
+}
+
+sub add($self, $newobject) {
+
+	my $newuuid=$self->uuid_v4();
+	my $newversion=$self->uuid_v4();
+
+	$newobject->{version}=$newversion;
+	$newobject->{uuid}=$newuuid;
+
+	my $uuid=$self->pg->db->insert('objects', {
+			uuid => $newuuid,
+			version => $newversion,
+			object => encode_json($newobject)
+		}, { returning => 'uuid' })->hash->{uuid};
+
+	my $storedobject=$self->get($uuid);
+
+	return $storedobject;
 }
 
 1;
---
+++ backend/t/20-put-object.t
@@ -0,0 +1,33 @@
+use Mojo::Base -strict;
+
+use Test::More;
+use Test::Mojo;
+use FindBin qw($Bin);
+
+use InvDB::TestHelper;
+my $idbt=new InvDB::TestHelper();
+$idbt->backend_conf_write("$Bin/../backend.conf");
+
+my $t = Test::Mojo->new('InvDB::Backend');
+
+$t->post_ok('/v1/object' => json => { type => "bar", attributes => { foo => [ "bar" ] }})
+	->status_is(200)
+	->json_is("/attributes/foo/0" => "bar")
+	->json_has("/uuid")
+	->json_has("/version");
+
+# Must fail - New object are not allowed to contain a uuid or version
+$t->post_ok('/v1/object' => json => { version => "1", type => "bar", attributes => { } })
+	->status_is(400);
+$t->post_ok('/v1/object' => json => { uuid => "1", type => "bar", attributes => { } })
+	->status_is(400);
+
+# Must have some content e.g. attributes
+$t->post_ok('/v1/object' => json => { type => "bar" })
+	->status_is(400);
+
+# Must have a type
+$t->post_ok('/v1/object' => json => { attributes => {} })
+	->status_is(400);
+
+done_testing();
